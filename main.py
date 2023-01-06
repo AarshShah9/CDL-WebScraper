c28fbf49670a5392b999b3e362b23d7f43e798ef
AarshShah9 <aarshshah99@gmail.com>
2023-01-05 21:56:31 -0600

    Added basic webscraper to get links of companies

---
+++ main.py
@@ -0,0 +1,56 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def main():
+
+    # go to base website
+    # find all company sub links by navigating to every div with class "unit"
+    # go to each sub link
+    # find company name and website link from the sublink
+
+    loop_counter = 0
+    locations = ['vancouver']
+    streams = ['', ]
+
+    base_url = f"https://creativedestructionlab.com/companies/"
+
+    # base_url = f"https://creativedestructionlab.com/companies/?location=vancouver"
+    # base_url = f"https://creativedestructionlab.com/companies/?stream={streams[0]}&location={locations[0]}"
+
+    for stream in streams:
+        for location in locations:
+
+            url = base_url + f"?location={location}"
+            source = requests.get(url).text
+
+            soup = BeautifulSoup(source, 'lxml')
+            mydivs = soup.find_all("div", {"class": "unit"})
+
+            for div in mydivs:
+                loop_counter += 1
+                cdlcompany_url = div.find('a')
+
+                if (
+                    (cdlcompany_url is not None)
+                    and (cdlcompany_url['href'] is not None)
+                    and cdlcompany_url['href'].startswith(
+                        'https://creativedestructionlab.com/companies/'
+                    )
+                ):
+                    cdlcompany_url = cdlcompany_url['href']
+                    company_source = requests.get(cdlcompany_url).text
+
+                    company_soup = BeautifulSoup(company_source, 'lxml')
+                    company_website = company_soup.find(
+                        'a', {'class': 'company-website-link'})
+                    company_name = company_soup.find(
+                        'h2', {'class': 'c-primary'})
+
+                    if (company_website is not None) and (company_name is not None):
+                        print(
+                            f"{company_website['href']} - {company_name.text} - Counter: {loop_counter}\n")
+
+
+if __name__ == '__main__':
+    main()
